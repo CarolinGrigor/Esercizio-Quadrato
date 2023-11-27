ce3424a83cc35b8fc6fa3d1e800b003cb6479525
CarolinGrigor <125268417+CarolinGrigor@users.noreply.github.com>
2023-11-27 12:11:12 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Quadrato l = new Quadrato();
+
+        Console.WriteLine("Inserisci lato");
+        l.lato = float.Parse(Console.ReadLine());
+
+        Console.WriteLine("Area = " + l.AreaQuadrato());
+
+        Console.ReadKey();
+    }
+}
+
+public class Quadrato
+{
+    public float lato { get; set; }
+
+    public float AreaQuadrato()
+    {
+        return lato * lato;
+
+    }
+}
